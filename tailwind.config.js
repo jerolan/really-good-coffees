0d1c0fe4d64050e2f18bf5ef047e97b4be7a139e
Jerome Olvera <jolveras@gbm.com.mx>
2021-01-24 19:07:27 -0600

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./pages/**/*.js", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
