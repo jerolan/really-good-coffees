9e5e8a441c75c44572f3ec2fd01fd6adafee697e
Jerome Olvera <jolveras@gbm.com.mx>
2021-02-08 09:16:02 -0600

    Improve fansy text

--- rgc/fansy-text.js
+++ rgc/fansy-text.js
@@ -1,25 +1,15 @@
 export default function FancyText({ children }) {
   return (
-    <div
-      className="relative italic"
-      style={{ marginBottom: 4, marginRight: 8 }}
-    >
-      {children}
-      <span className="absolute inset-0 text-dark dark:text-light z-50">
-        {children}
-      </span>
+    <p className="relative italic" style={{ marginBottom: 8, marginRight: 8 }}>
       <span
-        className="absolute inset-0 text-secondary"
-        style={{ left: 4, top: 2 }}
+        className="text-dark dark:text-light"
+        style={{
+          textShadow:
+            "4px 4px 0px #3872FF, 8px 8px 0px RGBA(56, 114, 255, 0.5);",
+        }}
       >
         {children}
       </span>
-      <span
-        className="absolute inset-0 text-secondary opacity-50"
-        style={{ left: 8, top: 4 }}
-      >
-        {children}
-      </span>
-    </div>
+    </p>
   );
 }
