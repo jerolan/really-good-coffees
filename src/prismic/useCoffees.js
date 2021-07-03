1da62b723e54e2284c1362d304524609c61e00dd
Jerome Olvera <jerome.olvera@gmail.com>
2021-07-03 15:53:04 -0500

    Use useSWRInfinite loading

--- src/prismic/useCoffees.js
+++ src/prismic/useCoffees.js
@@ -1,4 +1,5 @@
-import useSWR, { useSWRInfinite } from "swr";
+import { useEffect, useMemo } from "react";
+import { useSWRInfinite } from "swr";
 
 import fetchCoffees from "./fetchCoffees";
 
@@ -7,12 +8,25 @@ const PAGE_SIZE = 5;
 
 // useCoffees is a hook that will fetch the coffees from the Prismic API
 export default function useCoffees() {
-  const { data, error } = useSWR(
-    ["coffeees", INITIAL_PAGE, PAGE_SIZE],
-    (_, pageSize, page) => {
-      return fetchCoffees({ page, pageSize });
-    }
-  );
+  const {
+    data: pages,
+    error,
+    size,
+    setSize,
+  } = useSWRInfinite(getKey, (_, page, pageSize) => {
+    return fetchCoffees({ page, pageSize });
+  });
 
+  // recursive increment page size
+  useEffect(() => {
+    setSize((prevSize) => size + 1);
+  }, [pages, setSize]);
+
+  const data = useMemo(() => pages?.flat(), [pages]);
   return [data, error];
+}
+
+function getKey(pageIndex, previousPageData) {
+  if (previousPageData != null && !previousPageData.length) return null;
+  return ["coffeees", INITIAL_PAGE + pageIndex, PAGE_SIZE];
 }
